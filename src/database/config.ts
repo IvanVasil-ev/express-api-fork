5f90f4cda1ce707f8926afb33951ad251b042968
Ivan Vasilev <i.vasilev@dunice.net>
2022-04-26 18:29:03 +0300

    Initial commit

---
+++ src/database/config.ts
@@ -0,0 +1,31 @@
+import { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } from '../configuration';
+
+export default {
+  development: {
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    username: DB_USER,
+    host: DB_HOST,
+    port: DB_PORT,
+    logging: true,
+    dialect: 'postgres',
+  },
+  test: {
+    database: `${DB_DATABASE}_test`,
+    password: DB_PASSWORD,
+    username: DB_USER,
+    host: DB_HOST,
+    port: DB_PORT,
+    logging: false,
+    dialect: 'postgres',
+  },
+  production: {
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    username: DB_USER,
+    host: DB_HOST,
+    port: DB_PORT,
+    logging: false,
+    dialect: 'postgres',
+  },
+};
